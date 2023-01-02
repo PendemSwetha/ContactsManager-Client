b479afa0e9539c2fd54b7aeb55c08e887b3acdb1
amireddy-lavanya <amireddy.lavanya@gmail.com>
2023-01-02 13:25:17 +0530

    ToolTip Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
-
-import './App.css';
-
+import React from 'react';
+import './index.css';
+import { Provider } from './components/axios/axioscontext';
+import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
+import { Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ContactPage from './components/ContactPages';
 function App() {
+  const token=localStorage.getItem("token")
+  console.log(token)
   return (
-    <div className="App">
-     <h1>hello</h1>
+    <div>
+      <BrowserRouter>
+        <Provider>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/contacts" element={token ? <ContactPage /> : <Navigate replace to={"/"} />} />
+          </Routes>
+        </Provider>
+    </BrowserRouter>
+      
     </div>
   );
 }
--- client/src/components/UserValidation/Logout/Logout.jsx
+++
@@ -1,9 +0,0 @@
-import {Link} from "react-router-dom";
-const Logout = ()=>{
-    return(
-        <div>
-            <Link to="/Signin"><button>Logout</button></Link>
-        </div>
-    )
-}
-export default Logout;
